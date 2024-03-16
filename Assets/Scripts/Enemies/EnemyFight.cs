98ddfe7319dfbfe979d6968bb291dcd0d6abaf17
TuT-Master <vjtnovak@gmail.com>
2024-03-16 22:38:18 +0100

    DungeonGenerator

--- Assets/Scripts/Enemies/EnemyFight.cs
+++ Assets/Scripts/Enemies/EnemyFight.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class EnemyFight : MonoBehaviour
 {
-
+    public enum Attitude
+    {
+        MeleeAgressive,
+        MeleeEvasive,
+        MeleeWandering,
+        MeleeStealth,
+        RangedStatic,
+        RangedWandering,
+        Placeable,
+    }
+    public Attitude attitude;
 }
